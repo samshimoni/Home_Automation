d2226119ecd10ab133745ef68f8ecc1192c1e9c0
Samuel Chemouny <samuelchemouny@Samuels-MacBook-Pro.local>
2020-12-01 18:00:21 +0200

    continuing working on logger

--- Logger.py
+++ Logger.py
@@ -1,22 +1,32 @@
 import logging
+import json
 
 from python_logging_rabbitmq import RabbitMQHandler
 
-logger = logging.getLogger('my_service')
-logger.setLevel(logging.DEBUG)
 
-rabbit = RabbitMQHandler(host='localhost',
-                         port=30000,
-                         exchange='logs',
-                         routing_key_format='logs-api-1',
-                         username='guest',
-                         password='guest')
-logger.addHandler(rabbit)
+class Logger:
+    def __init__(self, service_name):
+        f = open('cfg.json',)
+        data = json.load(f)
+        print(data['Logger'])
+        self.name = service_name
+        self.logger = logging.getLogger('hello')
+        self.logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(levelname)s | %(asctime)s | %(message)s')
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+        rabbit = RabbitMQHandler(host='localhost',
+                                 port=[0],
+                                 exchange='logs',
+                                 routing_key_format='logs-api-1',
+                                 username='guest',
+                                 password='guest')
+
+        self.logger.addHandler(rabbit)
+
+        formatter = logging.Formatter('%(levelname)s | %(asctime)s | %(message)s')
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
 
+logger = Logger('logger')
