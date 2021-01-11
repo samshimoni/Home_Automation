4d19c604519fe3d22cbd25c3d0100156a34a7cda
Samuel <samshimoni@gmail.com>
2021-01-11 14:32:52 -0500

    automation the creatin and binding of the logging qeueue and exchage

---
+++ PlantWatering/init_rabbit.py
@@ -0,0 +1,18 @@
+import pika
+import cfg_watering
+
+'''
+    this script simply initiate the queue exchange and bind them together 
+'''
+
+cfg = cfg_watering.Cfg()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.rabbitHost, port=cfg.rabbitPort))
+
+channel = connection.channel()
+
+channel.queue_declare(queue=cfg.rabbitQueue)
+
+channel.exchange_declare(exchange=cfg.rabbitExchange, exchange_type=cfg.rabbitExchangeType)
+
+channel.queue_bind(exchange=cfg.rabbitExchange, queue=cfg.rabbitQueue, routing_key=cfg.rabbitRoutingKey)
